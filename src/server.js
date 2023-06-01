1093fbc905810a0d4ad7bb6b5c554d135492bd8c
Daniel Sarmento <danielsarmento2@hotmail.com>
2023-06-01 11:41:29 -0300

    Ajuste de repetição para 1hr

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(routes)
 
-cron.schedule('0-59/15 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
+cron.schedule('45 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
     let hora = new Date
     console.log(`Executando rotina Programada...${hora.getHours()}:${hora.getMinutes()}`)
     axios.get(`${env.URL_SEND}`)
