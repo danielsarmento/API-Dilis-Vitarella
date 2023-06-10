6be5443e3b071c6243c9e35c2bfa052bc23c3237
danielsarmento <danielsarmento2@hotmail.com>
2023-06-10 19:41:31 -0300

    ajuste de log

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(routes)
 
 cron.schedule('45 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
     let hora = new Date
-    console.log(`Executando rotina Programada...${hora.getHours()}:${hora.getMinutes()}`)
+    console.log(`Executando rotina Programada...${hora.getHours()}:${hora.getMinutes()} | ${hora.getDate()}/${hora.getMonth()}/${hora.getFullYear()}`)
     axios.get(`${env.URL_SEND}`)
 })
 
