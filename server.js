1a3bc71f8e5cec7c36ba48f7d03aaf3b12c0ecd5
danielsarmento <danielsarmento2@hotmail.com>
2023-06-12 17:52:30 -0300

    ajustes porta

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cron from 'node-cron'
 import axios from 'axios'
-import { routes } from './http/routes/routes.js'
-import { env } from './env/index.js'
+import { routes } from './src/http/routes/routes.js'
+import 'dotenv/config'
+//import { env } from './src/env/index.js'
 import swaggerUi from 'swagger-ui-express'
-import swaggerFile from './swagger.json' assert { type: "json" };
+import swaggerFile from './src/swagger.json' assert { type: "json" };
 import cors from 'cors'
 
+const PORT = process.env.PORT || 4444
 export const app = express()
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
@@ -19,9 +21,9 @@ app.use(routes)
 cron.schedule('0-59/30 0-23 1-31 6 mon-tue-wed-thu-fri-sat-sun', () => {
     let hora = new Date
     console.log(`Executando rotina Programada...${hora.getHours()}:${hora.getMinutes()} | ${hora.getDate()}/${hora.getMonth()}/${hora.getFullYear()}`)
-    axios.get(`${env.URL_SEND}`)
+    axios.get(`http://45.179.88.29:4444/register`)
 })
 
-app.listen(4444, () => {
+app.listen(PORT, () => {
     console.log('HTTP server running at PORT: 4444')
 })
